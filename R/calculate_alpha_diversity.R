73dfcfec659d516ba1273018b66f653b28ed522e
Martin Mortensen <MSMortensen@users.noreply.github.com>
2023-02-10 16:50:16 +0100

    test_variables added

--- R/calculate_alpha_diversity.R
+++ R/calculate_alpha_diversity.R
@@ -4,7 +4,7 @@
 #' @param pobject A \code{phyloseq} object.
 #' @param ntables Number or rarefactions to perform. Default 100
 #' @param depth Rarefaction depth at which the alpha diversity is calculated. Default 90 \% of the lowest sample depth
-#' @param methods Which alpha diversity metrics to calculate. Default c("Observed","Chao1","FaithPD","Shannon")
+#' @param INDECES Which alpha diversity metrics to calculate. Default c("Observed","Chao1","FaithPD","Shannon")
 #' @param seedstart Random seed to use. Default 500
 #' @param verbose Logical, indicating if the current sample being rarefied should be reported during calculation. Default FALSE
 #'
@@ -12,10 +12,10 @@
 #' @export
 #'
 #' @examples
-calculate_alpha_diversity <- function(pobject, ntables=100, depth = round(min(sample_sums(pobject))*0.9), methods=c("Observed","Chao1","FaithPD","Shannon"), seedstart=500, verbose=FALSE) {
+calculate_alpha_diversity <- function(pobject, ntables=100, depth = round(min(sample_sums(pobject))*0.9), INDECES=c("Observed","Chao1","FaithPD","Shannon"), seedstart=500, verbose=FALSE) {
   require("vegan")
   loadNamespace("phyloseq")
-  if (grepl("FaithPD", methods)) require("picante")
+  if (sum(grepl("FaithPD", INDECES)) > 0) require("picante")
 
   # remove samples below depth
   phy.use <- prune_samples(sample_sums(pobject) >= depth, pobject )
@@ -35,9 +35,9 @@ calculate_alpha_diversity <- function(pobject, ntables=100, depth = round(min(sa
   # Table to output alpha diversity table
   Alpha_diversity = data.frame(row.names = row.names(otu.tab))
 
-  for (i in seq(length(methods))){
-    Alpha_diversity[,methods[i]] <- numeric(length = nrow(otu.tab))
-    Alpha_diversity[,paste0(methods[i],"_sd")] <- numeric(length = nrow(otu.tab))
+  for (i in seq(length(INDECES))){
+    Alpha_diversity[,INDECES[i]] <- numeric(length = nrow(otu.tab))
+    Alpha_diversity[,paste0(INDECES[i],"_sd")] <- numeric(length = nrow(otu.tab))
   }
 
   # Run each sample separately
@@ -57,25 +57,25 @@ calculate_alpha_diversity <- function(pobject, ntables=100, depth = round(min(sa
     # Calculate Observed richness, Chao1, and ACE.
     adiv <- data.frame(t(estimateR(rare_tab)))
 
-    if ("Observed" %in% methods){
+    if ("Observed" %in% INDECES){
       # Save mean and sd of observed richness
       Alpha_diversity$Observed[z] <- mean(adiv$S.obs)
       Alpha_diversity$Observed_sd[z] <- sd(adiv$S.obs)
     }
 
-    if ("Chao1" %in% methods){
+    if ("Chao1" %in% INDECES){
       # Save mean and sd of observed richness
       Alpha_diversity$Chao1[z] <- mean(adiv$S.chao1)
       Alpha_diversity$Chao1_sd[z] <- sd(adiv$S.chao1)
     }
 
-    if ("ACE" %in% methods){
+    if ("ACE" %in% INDECES){
       # Save mean and sd of observed richness
       Alpha_diversity$ACE[z] <- mean(adiv$se.ACE)
       Alpha_diversity$ACE_sd[z] <- sd(adiv$se.ACE)
     }
 
-    if ("Shannon" %in% methods){
+    if ("Shannon" %in% INDECES){
       # Calculate observed richness for each rep of sample z
       adiv <- vegan::diversity(rare_tab, index = "shannon")
 
@@ -84,7 +84,7 @@ calculate_alpha_diversity <- function(pobject, ntables=100, depth = round(min(sa
       Alpha_diversity$Shannon_sd[z] <- sd(adiv)
     }
 
-    if ("Simpson" %in% methods){
+    if ("Simpson" %in% INDECES){
       # Calculate observed richness for each rep of sample z
       adiv <- diversity(rare_tab, index = "simpson")
       # Save mean and sd of observed richness
@@ -92,7 +92,7 @@ calculate_alpha_diversity <- function(pobject, ntables=100, depth = round(min(sa
       Alpha_diversity$Simpson_sd[z] <- sd(adiv)
     }
 
-    if ("Evenness" %in% methods){
+    if ("Evenness" %in% INDECES){
       # Calculate observed richness for each rep of sample z
       sha <- diversity(rare_tab, index = "shannon")
       obs <- rowSums(rare_tab != 0)
@@ -102,7 +102,7 @@ calculate_alpha_diversity <- function(pobject, ntables=100, depth = round(min(sa
       Alpha_diversity$Evenness_sd[z] <- sd(adiv)
     }
 
-    if ("FaithPD" %in% methods){
+    if ("FaithPD" %in% INDECES){
       colnames(rare_tab) <- taxa_names(pobject)
       # Calculate Faith Phylogenetic distance for each rep of sample z
       tmp <- pd(rare_tab, phy_tree(pobject), include.root = T)
